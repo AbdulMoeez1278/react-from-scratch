c025086ac1d3d91a3f0252f9a81159fd0f4c4798
Abdul Moeez <abdulmoeez1278@gmail.com>
2026-02-24 15:02:54 +0500

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,41 @@
+// JSX in React.js
+
+function User() {
+  return;
+  <div>
+    <h1>Hello World</h1>
+    <p>This is just a simple ```Hello World``` game.</p>
+  </div>;
+}
+
+export default User;
+
+User.addEventListener("click", () => {
+  setTimeout(() => {
+    console.log("True! It's just a simple game");
+  }, 3000);
+});
+
+const element = <h1>Hello Moeez </h1>;
+
+<h1>Hello World</h1>;
+
+return (
+  <div>
+    <h1>Hello</h1>
+    <p>World</p>
+  </div>
+);
+
+{
+  /* <>
+    <h1>Hello</h1>
+    <p>World</p>
+</> */
+}
+
+{
+  /* <h1>{5+5}</h1>
+<h1>{name.toUpperCase()}</h1>
+<h1>{isLoggedIn ? "Welcome" : Login}</h1> */
+}
