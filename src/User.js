92e375a8b88525b68bd7642cade2fd8fafb86c42
Abdul Moeez <abdulmoeez1278@gmail.com>
2026-03-04 13:25:23 +0500

    Update User.js

--- src/User.js
+++ src/User.js
@@ -52,7 +52,87 @@ const users = ["Ali", "Ahmed", "Moeez", "Rameez"];
       (<li key={index}>{user}</li>),
       (<li key={index}>{user}</li>),
       (<li key={index}>{user}</li>),
+      (<li key={index}>{user}</li>),
+      (<li key={index}>{user}</li>),
       (<li key={index}>{user}</li>)
     ),
   )}
 </ul>;
+
+// ------------------- //
+// Basic Props Example
+
+// Child Component
+function User(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
+// Parent Component
+function App() {
+  return <User name="Moeez" />;
+}
+
+// App();
+
+// ------------------- //
+// Destructuring Props
+function User({ name }) {
+  return <h1>Hello {name}</h1>;
+}
+
+// ---------------------- //
+// Passing Multiple Props
+<User name="Abdul Moeez" age={24} city="Lahore" />;
+
+// Child
+function User({ name, age, city }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <p>{age}</p>
+      <p>{city}</p>
+    </>
+  );
+}
+
+// ----------- //
+// Uses of JSX
+const name = "Abdul Moeez";
+const greeting = <h1>Hello, {name}!</h1>;
+console.log(greeting);
+
+// ------------- //
+// Default Props
+function User({ name = "moizycodes" }) {
+  return <h1>Hello {name}</h1>;
+}
+
+{
+  /* <User />; */
+}
+
+// ------------------------------------- //
+// Real Example (Professional Structure)
+function Card({ title, description, price }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <h3>${price}</h3>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    
+    <>
+      <Card
+        title="React Course"
+        description="Learn React from Scratch"
+        price={99}
+      />
+      <Card title="JS Course" description="Master JavaScript" price={79} />
+    </>
+  );
+}
